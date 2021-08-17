6c89ab64b7e18e940d6ecdcdd861dddb2f1f85dd
lakshman-uptycs <84305174+lakshman-uptycs@users.noreply.github.com>
2021-08-17 15:05:27 +0530

    CLD-1149: AWS Integration: Update CloudFormation template and Terraform script for Kinesis permissions (#1)
    * Add Kinesis permissions
    
    * Add servicecatalog permissions

--- modules/iam-profile/main.tf
+++ modules/iam-profile/main.tf
@@ -68,7 +68,13 @@ resource "aws_iam_policy" "ReadOnlyPolicy" {
               "sqs:ListQueueTags",
               "codepipeline:ListTagsForResource",
               "codepipeline:GetPipeline",
-              "ds:ListTagsForResource"
+              "ds:ListTagsForResource",
+              "kinesis:GetShardIterator",
+              "kinesis:GetRecords",
+              "kinesis:DescribeStream",
+              "servicecatalog:SearchProducts",
+              "servicecatalog:DescribeProduct",
+              "servicecatalog:DescribePortfolio"
             ],
             "Resource": "*"
         }
