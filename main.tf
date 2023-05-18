10e6fa72742f2a5262f45e8f60811fdc81606378
lakshman-uptycs <llukalapu@uptycs.com>
2023-05-18 15:40:42 +0530

    Revert "CSM-7595: remove region dependency (#17)"
    This reverts commit 39c197b8a307b72cee2e1a8c6c4da01caa01dbd1.

--- main.tf
+++ main.tf
@@ -1,27 +1,16 @@
-data "aws_region" "current" {}
-
-module "cloud_trail_bucket_details" {
-  source          = "./bucket_details"
-  resource_region = var.cloudtrail_s3_bucket_region != "" ? var.cloudtrail_s3_bucket_region : data.aws_region.current.id
-  bucket_name     = var.cloudtrail_s3_bucket_name
+data "aws_s3_bucket" "vpc_log_bucket_arn" {
+  count  = var.vpc_flowlogs_bucket_name != "" ? 1 : 0
+  bucket = var.vpc_flowlogs_bucket_name
 }
 
-module "vpc_flow_log_bucket_details" {
-  source          = "./bucket_details"
-  resource_region = var.vpc_flowlogs_bucket_region != "" ? var.vpc_flowlogs_bucket_region : data.aws_region.current.id
-  bucket_name     = var.vpc_flowlogs_bucket_name
+data "aws_s3_bucket" "cloudtrail_log_bucket_arn" {
+  count  = var.cloudtrail_s3_bucket_name != "" ? 1 : 0
+  bucket = var.cloudtrail_s3_bucket_name
 }
 
-module "kinesis_stream_deatils" {
-  source                = "./kinesis_details"
-  kinesis_stream_region = var.kinesis_stream_region != "" ? var.kinesis_stream_region : data.aws_region.current.id
-  kinesis_stream_name   = var.kinesis_stream_name
-}
-
-locals {
-  cloudtrail_log_bucket_arn = var.cloudtrail_s3_bucket_name != "" ? module.cloud_trail_bucket_details.bucket_arn : null
-  vpc_log_bucket_arn        = var.vpc_flowlogs_bucket_name != "" ? module.vpc_flow_log_bucket_details.bucket_arn : null
-  kinesis_stream_arn        = var.kinesis_stream_name != "" ? module.kinesis_stream_deatils.kinesis_stream_arn : null
+data "aws_kinesis_stream" "kinesis_stream_arn" {
+  count = var.kinesis_stream_name != "" ? 1 : 0
+  name  = var.kinesis_stream_name
 }
 
 module "instance_profile" {
@@ -29,8 +18,8 @@ module "instance_profile" {
   resource_prefix           = var.resource_prefix
   aws_account_id            = var.aws_account_id
   external_id               = var.external_id
-  cloudtrail_log_bucket_arn = local.cloudtrail_log_bucket_arn
-  vpc_log_bucket_arn        = local.vpc_log_bucket_arn
-  kinesis_stream_arn        = local.kinesis_stream_arn
+  cloudtrail_log_bucket_arn = var.cloudtrail_s3_bucket_name != "" ? data.aws_s3_bucket.cloudtrail_log_bucket_arn[0].arn : null
+  vpc_log_bucket_arn        = var.vpc_flowlogs_bucket_name != ""  ? data.aws_s3_bucket.vpc_log_bucket_arn[0].arn : null
+  kinesis_stream_arn        = var.kinesis_stream_name != "" ? data.aws_kinesis_stream.kinesis_stream_arn[0].arn : null
   tags                      = var.tags
 }
